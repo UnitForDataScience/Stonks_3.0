74a74a2d6a31a453c48e0e408620d19eab1e8b5c
djlittle <42420410+djlittle@users.noreply.github.com>
2021-07-09 08:49:16 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,125 @@
+# -------------------------------------------------------------------------------
+# Name: main.py
+# Purpose: Pull data from twitter, perform sentiment analysis and pull stock data
+#
+# Author(s):    David Little
+#
+# Created:      04/26/2021
+# Updated:
+# Update Comment(s):
+#
+# TO DO:
+#
+# -------------------------------------------------------------------------------
+
+import requests
+import pandas as pd
+import datetime
+import regex as re
+from datetime import datetime, timedelta
+
+def get_data(tweet):
+    data = {
+        'id': tweet['id'],
+        'created_at': tweet['created_at'],
+        'text': tweet['text']
+    }
+    return data
+
+whitespace = re.compile(r"\s+")
+web_address = re.compile(r"(?i)http(s):\/\/[a-z0-9.~_\-\/]+")
+tesla = re.compile(r"(?i)@Tesla(?=\b)")
+user = re.compile(r"(?i)@[a-z0-9_]+")
+
+#------------------------------------- Twitter Pull  --------------------------------------------------------
+
+# setup the API request
+endpoint = 'https://api.twitter.com/2/tweets/search/recent'  # 'https://api.twitter.com/2/tweets/search/all'
+headers = {'authorization': f'Bearer {BEARER_TOKEN}'}
+params = {
+    'query': '(tesla OR tsla OR elon musk) (lang:en)',
+    'max_results': '100',
+    'tweet.fields': 'created_at,lang'
+        }
+
+dtformat = '%Y-%m-%dT%H:%M:%SZ'  # the date format string required by twitter
+
+# we use this function to subtract 60 mins from our datetime string
+def time_travel(now, mins):
+    now = datetime.strptime(now, dtformat)
+    back_in_time = now - timedelta(minutes=mins)
+    return back_in_time.strftime(dtformat)
+
+now = datetime.now()  # get the current datetime, this is our starting point
+last_week = now - timedelta(days=6)  # datetime one week ago = the finish line
+now = now.strftime(dtformat)  # convert now datetime to format for API
+
+df = pd.DataFrame()  # initialize dataframe to store tweets
+while True:
+    if datetime.strptime(now, dtformat) < last_week:
+        # if we have reached 6 days ago, break the loop
+        break
+    pre60 = time_travel(now, 60)  # get 60 minutes before 'now'
+    # assign from and to datetime parameters for the API
+    params['start_time'] = pre60
+    params['end_time'] = now
+    response = requests.get(endpoint,
+                            params=params,
+                            headers=headers)  # send the request
+    now = pre60  # move the window 60 minutes earlier
+    # iteratively append our tweet data to our dataframe
+    for tweet in response.json()['data']:
+        row = get_data(tweet)  # we defined this function earlier
+        df = df.append(row, ignore_index=True)
+
+#---------------------------------------------- Sentiment Model ------------------------------------------------------
+
+import flair
+sentiment_model = flair.models.TextClassifier.load('en-sentiment')
+
+# we will append probability and sentiment preds later
+probs = []
+sentiments = []
+clean_tweets = []
+timestamp = []
+
+for time in df['created_at']:
+    timestamp.append(datetime.strptime(time, '%Y-%m-%dT%H:%M:%S.%fZ').strftime('%Y-%m-%d %H:%M:%S'))
+
+for tweet in df['text']:
+# we then use the sub method to replace anything matching
+    tweet = whitespace.sub(' ', tweet)
+    tweet = web_address.sub('', tweet)
+    tweet = tesla.sub('Tesla', tweet)
+    tweet = user.sub('', tweet)
+    sentence = flair.data.Sentence(tweet)
+    sentiment_model.predict(sentence)
+    # extract sentiment prediction
+    probs.append(sentence.labels[0].score)  # numerical score 0-1
+    sentiments.append(sentence.labels[0].value)  # 'POSITIVE' or 'NEGATIVE'
+    clean_tweets.append(tweet)
+    # print(tweet)
+    # print(' ')
+
+# add probability and sentiment predictions to tweets dataframe
+df['text_clean'] = clean_tweets
+df['probability'] = probs
+df['sentiment'] = sentiments
+df['timestamp'] = timestamp
+
+#________________________________ Stock Data __________________________________________________________________
+
+import yfinance as yf
+
+tsla = yf.Ticker("TSLA")
+tsla_stock = tsla.history(
+    start=datetime.strptime(df['created_at'].min(),'%Y-%m-%dT%H:%M:%S.%fZ').strftime('%Y-%m-%d'),
+    end=datetime.strptime(df['created_at'].max(),'%Y-%m-%dT%H:%M:%S.%fZ').strftime('%Y-%m-%d'),
+    interval='1d'
+        ).reset_index()
+
+#________________________________ Export __________________________________________________________________
+
+df.to_csv('Sentiment.csv', index=False)
+tsla_stock.to_csv('Stocks.csv', index=False)
+
